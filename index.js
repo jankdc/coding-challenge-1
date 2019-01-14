2a737e5bcd5b1e8ea595d53bc27bacf47da092ae
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2019-01-14 09:55:27 +0000

    Integrate simulator

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const fs = require('fs')
 const readline = require('readline')
 
+const {
+  parsePosition,
+  parseDimensions,
+  parseInstructions
+} = require('./lib/input')
+
+const {
+  Simulator
+} = require('./lib/simulator')
+
 function main () {
   const filename = process.argv[2]
 
   const fileStream = fs.createReadStream(filename)
   const lineStream = readline.createInterface({ input: fileStream })
 
+  let index = 0
+  let shipPosition
+
+  const simulator = new Simulator()
+
   lineStream.on('line', line => {
-    console.log(line)
+    if (line.length === 0) {
+      return
+    }
+
+    if (index === 0) {
+      simulator.setDimensions(parseDimensions(line))
+    } else if (index % 2 === 1) {
+      shipPosition = parsePosition(line)
+    } else {
+      simulator.run(shipPosition, parseInstructions(line))
+    }
+
+    index += 1
   })
 }
 
